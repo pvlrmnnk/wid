7e388062c3875830cd0947b5a83f96139d5b8c2c
Pavlo Romanenko <mail@pvlrmnnk.com>
2022-05-21 19:11:00 +0300

    Update cmd, add simple logic to retrive targz content

--- cmd/wiid/main.go
+++ cmd/wiid/main.go
@@ -1,5 +1,52 @@
 package main
 
+import (
+	"archive/tar"
+	"compress/gzip"
+	"errors"
+	"io"
+	"log"
+	"os"
+)
+
 func main() {
-	//
+	if len(os.Args) != 2 {
+		fail(errors.New("wrong usage"))
+	}
+
+	fn := os.Args[1]
+	log.Println("File: ", fn)
+
+	fr, err := os.Open(fn)
+	if err != nil {
+		fail(err)
+	}
+	defer fr.Close()
+
+	gzr, err := gzip.NewReader(fr)
+	if err != nil {
+		fail(err)
+	}
+	defer gzr.Close()
+
+	list := make([]string, 0, 1000)
+
+	tr := tar.NewReader(gzr)
+	for {
+		th, err := tr.Next()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fail(err)
+		}
+		log.Println(th)
+		list = append(list, th.Name)
+	}
+
+	log.Println(list)
+}
+
+func fail(err error) {
+	log.Fatalln(err)
 }
